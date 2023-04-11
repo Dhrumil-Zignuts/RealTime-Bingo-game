051cbddbc3e4000aeda6a127391d96549e2a2370
Dhrumil-Zignuts <dhrumila@zignuts.com>
2023-04-11 15:31:32 +0530

    BingoVersion..1

--- public/index.js
+++ public/index.js
@@ -1,49 +1,209 @@
 $(document).ready(function () {
     const socket = io();
+    let myUsers
+
+    let username
+    let room
+    let connectedUsers
     $('#createRoom').on('click', function () {
+        username = $('#createUserName').val()
+        room = $('#createRoomName').val()
+
         const createRoomData = {
-            username: $('#createUserName').val(),
-            room: $('#createRoomName').val()
+            username: username,
+            room: room
         }
-        console.log('createRoomdata: ', createRoomData);
         socket.emit('create-room', createRoomData)
     })
 
-    socket.on('room-created', (room, username) => {
-       
-        console.log(`${room} is Created by ${username}`);
-        const array = []
-        array.push(username)
-        localStorage.setItem("Dhrumil", JSON.stringify(array))
-        $('#tableBody').append(`
-        <tr>
-            <td>${username}</td>
-        </tr>`)
+    socket.on('room-created', (users) => {
+        $('#tableBody').empty();
+        for (let i = 0; i < users.length; i++) {
+            $('#tableBody').append(`
+                <tr>
+                    <td>${users[i].username}</td>
+                </tr>`)
+
+        }
         $('#exampleModal').modal('hide')
-         $('#home').css('display', 'none')
+        $('#home').css('display', 'none')
         $('#game').css('display', 'block')
     })
 
-    $('#joinRoom').on('click', function(){
+    $('#joinRoom').on('click', function () {
+        username = $('#joinUserName').val()
+        room = $('#joinRoomName').val()
         const joinUserData = {
-            username : $('#joinUserName').val(),
-            room : $('#joinRoomName').val()
+            username: username,
+            room: room
         }
-        console.log('joinUserData: ', joinUserData);
+        // 
         socket.emit('join-room', joinUserData)
     })
 
-    socket.on('room-joined', (room, username) => {
-       
-        console.log(`${room} is Joined by ${username}`);
-        const data = JSON.parse(localStorage.getItem('Dhrumil'))
-        localStorage.setItem('Dhrumil', JSON.stringify([...data, username]))
-        $('#tableBody').append(`
-        <tr>
-            <td>${username}</td>
-        </tr>`)
+    socket.on('room-joined', (users) => {
+        myUsers = users
+        $('#tableBody').empty();
+        for (let i = 0; i < users.length; i++) {
+            $('#tableBody').append(`
+                <tr>
+                    <td>${users[i].username}</td>
+                </tr>`)
+        }
         $('#exampleModal2').modal('hide')
-         $('#home').css('display', 'none')
+        $('#home').css('display', 'none')
         $('#game').css('display', 'block')
+        $('#jugad').val(JSON.stringify(users))
+
+
     })
+
+
+    function getTable() {
+        let arr = new Array(25);
+
+        // Fill the array with numbers from 1 to 25
+        for (let i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        // Shuffle the array using Fisher-Yates shuffle algorithm
+        for (let i = arr.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        // 
+
+        for (let i = 0; i < arr.length; i++) {
+            $('.main-grid').append(`
+                <button class="bingo-btn btn btn-secondary text-decoration-none blocks ${arr[i]}" id="${i}" data-id = ${arr[i]} > ${arr[i]} </button>
+            `)
+        }
+    }
+    getTable()
+
+    let clickedBtn = []
+    let combination = [
+        [0, 1, 2, 3, 4],
+        [0, 5, 10, 15, 20],
+        [5, 6, 7, 8, 9],
+        [10, 11, 12, 13, 14],
+        [15, 16, 17, 18, 19],
+        [20, 21, 22, 23, 24],
+        [1, 6, 11, 16, 21],
+        [2, 7, 12, 17, 22],
+        [3, 8, 13, 18, 23],
+        [4, 9, 14, 19, 24],
+        [0, 6, 12, 18, 24],
+        [4, 8, 12, 16, 20]
+    ]
+
+
+    let turn = 0
+    // let usersdata = $('#jugad').val()
+    // let userdata = eval(usersdata)
+    $('.bingo-btn').on('click', function () {
+        console.log(myUsers);
+        console.log(username);
+        console.log('This is a J: ', turn);
+        for (let i = 0 ; i < myUsers.length; i++) {
+            if (username === myUsers[turn].username) {
+                console.log('username: ', username);
+                console.log('userdata[i].username: ', myUsers[turn].username);
+                const id = $(this).data('id');
+                if( turn < myUsers.length - 1 ){
+                    turn = turn + 1
+                    const data = {
+                        id: id,
+                        username: username,
+                        room: room,
+                        a : turn
+                    }
+                    console.log('turn: ', turn);
+                    socket.emit('user-clicked', data)
+                }else{
+                    turn = 0
+                    console.log('turn: ', turn);
+                    const data = {
+                        id: id,
+                        username: username,
+                        room: room,
+                        a : turn
+                    }
+                    socket.emit('user-clicked', data)
+                }
+                break
+            }else{
+                continue
+            }
+        }
+    })
+
+
+    socket.on('user-move', (data) => {
+        console.log('data: ', data);
+        turn = data.a
+        $(`.${data.id}`).toggleClass('btn-secondary btn-success');
+        // $(`.${data.id}`).prop('disabled', true);
+        // 
+
+        // add index in array
+        const index = $(`.${data.id}`).attr('id')
+        console.log('index: ', index);
+        clickedBtn.push(index)
+
+
+        // add move of user in table 
+        $('#tableMoves').append(`
+            <tr>
+                <td>${data.username}</td>
+                <td>${data.id}</td>
+            </tr>
+        `)
+
+        let bingoCount = 0
+        for (let j = 0; j < combination.length; j++) {
+            let count = 0
+            for (let k = 0; k < combination[j].length; k++) {
+                for (let i = 0; i < clickedBtn.length; i++) {
+                    if (clickedBtn[i] == combination[j][k]) {
+                        count++
+                    } else {
+                        continue
+                    }
+                }
+                if (count == 5) {
+                    bingoCount++
+                } else if (count == 0 && k > 0) {
+                    break
+                }
+            }
+
+
+        }
+        if (bingoCount == 1) {
+            $('#bingoCount').html('B')
+        } else if (bingoCount == 2) {
+            $('#bingoCount').html('BI')
+        } else if (bingoCount == 3) {
+            $('#bingoCount').html('BIN')
+        } else if (bingoCount == 4) {
+            $('#bingoCount').html('BING')
+        } else if (bingoCount == 5) {
+            $('#bingoCount').html('BINGO')
+            winner = {
+                username: username,
+                room: room
+            }
+            socket.emit('winner', data)
+        }
+    })
+
+    socket.on('winner', (data) => {
+        $('#exampleModal3').modal('show')
+        $('#winner').html(`Congratulation Winner is ${data.username}`)
+    })
+
+
+
 })
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const {createServer} = require('http')
-const {Server} = require('socket.io')
+const mongoose = require('mongoose')
+const { createServer } = require('http')
+const { Server } = require('socket.io')
 const path = require('path')
+const { create } = require('domain')
 
 const app = express()
 const httpServer = createServer(app)
@@ -14,24 +16,72 @@ app.use(express.static(path.join(__dirname, 'public')))
 // app.get('/', (req, res) => {
 //     res.sendFile(__dirname, 'index.html');
 // });
-
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
     console.log('Connection Done..!!');
-
-    socket.on('create-room', ({username, room})=>{
+    socket.on('create-room', async ({ username, room }) => {
         socket.join(room)
-        io.to(room).emit('room-created', room ,username)
+        const data = {
+            username: username,
+            room : room,
+            socketID : socket.id
+        }
+        try{
+            const user = await MyModel.create(data)
+            const users = await MyModel.find({ room : room})
+            io.to(room).emit('room-created', users)
+        }catch(err){
+            console.log(err);
+        }
+
+    })
+    socket.on('join-room', async ({ username, room }) => {
+        socket.join(room)
+        const data = {
+            username: username,
+            room : room,
+            socketID : socket.id
+        }
+        try{
+            const user = await MyModel.create(data)
+            const users = await MyModel.find({ room : room})
+            io.to(room).emit('room-joined', users)
+        }catch(err){
+            console.log(err);
+        }
     })
 
-    socket.on('join-room', ({username, room})=>{
-        socket.join(room)
-        io.to(room).emit('room-joined', room , username)
+    socket.on('user-clicked', (data) => {
+        console.log('data: ', data);
+        io.to(data.room).emit('user-move', data)
     })
+
+    socket.on('winner', (data) => {
+        io.to(data.room).emit('winner', data)
+    })
+
+    // socket.on("disconnect", async (reason) => {
+    //     console.log('reason: ', reason);
+    //     try{
+    //         const disconnectedUser = await MyModel.findOne({socketID : socket.id})
+    //         const deleteUser = await MyModel.deleteOne({socketID : socket.id})
+    //         console.log('disconnectedUser: ', disconnectedUser);
+    //         const users = await MyModel.find({ room : disconnectedUser.room})
+    //         console.log('users: ', users);
+    //         io.to(disconnectedUser.room).emit('room-joined', users)
+    //     }catch(err){
+    //         console.log(err);
+    //     }
+    //   });
 })
+
+
+const MyModel = mongoose.model('Users', new mongoose.Schema({ username: String, room : String, socketID : String }));
+mongoose.connect('mongodb+srv://dhrumilZignuts:Zignuts123@zignuts-technology.zveo5bz.mongodb.net/BingoUsers?retryWrites=true&w=majority');
 
 
 const PORT = process.env.PORT || 3000
 
-httpServer.listen(PORT, ()=>{
+httpServer.listen(PORT, () => {
+
     console.log(`Server is Running on port no : ${PORT}`);
 })
